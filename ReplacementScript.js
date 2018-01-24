80fe1c182e67841a49268930aac57a76ca0e3968
Jerrgree <jeremyrgreenburg@gmail.com>
2018-01-23 23:52:39 -0600

    Documented all scripts

---
+++ ReplacementScript.js
@@ -0,0 +1,32 @@
+const normalScript = "*This Is A*: //Normal Ship// \n\nThe following additional rules are in effect: \n//- Day 1 Rampage Ban// 'n//- Day 1 Apron Ban//";
+const mushScript = "**This Is A:** //Farm Ship// \n\nIdentify yourselves to the rest of the crew. \nAs mush, your first priority should be to make sure Terrence gets infected, as well as the cat. (Avoid infecting the cat until either lenses are done or the Targetting Assist has been bought.)\n If you're Terrence or Ian, one of you should transfer into Raluca, or into someone who REALLY wants to be Raluca. Beyond that, transferring is left to your best judgement. \nAnd remember, don't hog the spores!";
+const jinScript = "**This Is A:** //Farm Ship// \n\nMake sure to take Leader \n-If you're a pilot, make sure to destroy trader ships. (Keep in mind you must bring them down to 0 before the end of the cycle.) \n-Set up the Swedish Sofa in the Refectory \n"
+const friedaScript = "**This Is A:** //Farm Ship// \n\n-Set up the Swedish Sofa in the Refectory \n-If you're a pilot, make sure to destroy trader ships. (Keep in mind you must bring them down to 0 before the end of the cycle.)";
+const KuanScript = "**This Is A:** //Farm Ship// \n\n-Make sure to take Leader \n-If Tech, get PILGRED set up \n-Set up Swedish Sofa in the Refectory";
+const janiceScript = "**This Is A:** //Farm Ship// \n\n-Keep track of who is left Human \n-Set up Swedish Sofa in the Refectory";
+const rolandScript = "**This Is A:** //Farm Ship// \n\n-Make sure to destroy trader ships. (Keep in mind you must bring them down to 0 before the end of the cycle.) \n-Buy Targetting Assist or SNC Kitchen if available \n-set up the Swedish Sofa in the Refectory";
+const huaScript = "**This Is A:** //Farm Ship// \n\n-Make sure to destroy trader ships. (Keep in mind you must bring them down to 0 before the end of the cycle.) \n-If you're a Tech, get PILGRED set up \n-Buy Targetting Assist if available. \n-Plants should go in Refectory";
+const paolaScript = "**This Is A:** //Farm Ship// \n\n-Swedish Sofa should go in the Refectory \n-Buy SNC kitchen if available. \n-Keep in mind, SNC kitchen can make you dirty. It's up to you if you feel can use it while decoding bases and updating NERON.";
+const stephenScript = "**This Is A:** //Farm Ship// \n\n-Don't use Apprentice until you know you're the final Stephen \n-Consider Leader or Pilot for Apprentice \n-Build Swedish Sofa in the Refectory";
+const ianScript = "**This Is A:** //Farm Ship// \n\n-Don't forget Green Thumb transfers! \n-Plants should end up in the Refectory \n-Build Swedish Sofa in the Refectory";
+const chunScript = "**This Is A:** //Farm Ship// \n\n-You get dibs on the initial Magebook \n-Don't forget to take Mankinds Only Hope \n-Build Swedish Sofa in the Refectory";
+const ralucaScript = "**This Is A:** //Farm Ship// \n\n-Finish PILGRED immediately \n-Build Swedish Sofa in the Refectory";
+const gioeleScript = "**This Is A:** //Farm Ship// \n\n-Buy SNC Kitchen if available \nBuild Swedish Sofa in the Refectory";
+const eleeshaScript = "**This Is A:** //Farm Ship// \n\n-Buy SNC Kitchen if available \n-If Tech, Help with PILGRED immediately \n-Build Swedish Sofa in the Refectory";
+const terrenceScript = "**This Is A:** //Farm Ship// \n\n-Don't Forget Robotics Expert transfer! \n-Build Swedish Sofa in Refectory \n-Buy Targetting Assist or SNC Kitchen if available";
+const derekScript = "**This Is A:** //Farm Ship// \n\n-Build Swedish Sofa in the Refectory \n-Be sure to check HP of folks not yet in the refectory \n-Check on plants \n-Build Sofa in Refectory";
+const andieScript = "**This Is A:** //Farm Ship// \n\n-If you're a pilot, make sure to destroy trader ships. (Keep in mind you must bring them down to 0 before the end of the cycle.) \n-Buy Targetting Assist if available \n-Build Sofa in Refectory";
+
+//x = $('.rpbloc > h3')
+playerScripts = $('.rpbloc');
+playerScripts[i][1]; // h3, the character that the script pertains to
+playerScripts[0].children[4]; // The textarea for the script
+playerScripts[0].children[7].children[0].children[0].children[0]; // The link to update the script
+
+playerScripts = $('.rpbloc');
+playerScripts[0].children[4].innerHTML = normalScript;
+playerScripts[0].children[7].children[0].children[0].children[0].click();
+
+playerScripts = $('.rpbloc');
+playerScripts[1].children[4].innerHTML = "This is a test scri;pt2";
+playerScripts[1].children[7].children[0].children[0].children[0].click();
